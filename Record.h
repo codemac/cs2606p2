d57e22ca7ebbc5063e756766109b7b3c58c1b1bd
codemac <codemac@a5beea13-9b1e-0410-9abb-f3fe362f9339>
2006-09-29 19:41:15 +0000

    Added record class

---
+++ Record.h
@@ -0,0 +1,47 @@
+#ifndef RECORD_H
+#define RECORD_H
+
+#include <string>
+
+using namespace std;
+
+class Record {
+	private:
+		string ttitle,				///	record title
+			   ddatetime,			///	date in format YYMMDDHHmm
+			   ddescription;			/// record description
+
+		unsigned int llength,		///	length in minutes
+					 ccost,			///	cost in whole dollar amounts
+					 IID;			///	unique ID
+
+		unsigned short int xx,		///	coordinate for the X axis
+					       yy;		///	coordinate for the Y axis
+
+	public:
+		string title();
+		void title(const string& t);
+
+		string date();
+		void date(const string& d);
+
+		string description();
+		void description(const string& d);
+
+		unsigned int length();
+		void length(const unsigned int& l);
+
+		unsigned int cost();
+		void cost(const unsigned int& c);
+
+		unsigned int ID();
+		void ID(const unsigned int& i);
+
+		unsigned short int x();
+		void x(const unsigned short int& x);
+
+		unsigned short int y();
+		void y(const unsigned short int& y);
+
+		unsigned short int* coords();
+}
