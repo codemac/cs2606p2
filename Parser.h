59b51b867354fe9cde399832792c8b310544a53a
codemac <codemac@a5beea13-9b1e-0410-9abb-f3fe362f9339>
2006-10-03 19:24:42 +0000

    Fixed simple mistakes, added some meat to Parser.h, no functionality though

--- Parser.h
+++ Parser.h
@@ -2,12 +2,23 @@
 #define PARSER_H
 
 #include <string>
+#include <iostream>
 
 using namespace std;
 
+/**
+ * This class takes stdin, and gives back an array of strings in a pointer
+ * so that some other interpreter class can take the tokenized results and 
+ * perform some function on them
+ */
 class Parser
 {
 	private:
+		istream in;
 
 	public:
-}
+		Parser(const istream &in);
+		Parser();
+		~Parser();
+};
+#endif
