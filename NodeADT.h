f22c4ead375e953a78c18285f25466b93c1ba7ce
codemac <codemac@a5beea13-9b1e-0410-9abb-f3fe362f9339>
2006-10-05 19:47:39 +0000

    Attempting dump, not so much

--- NodeADT.h
+++ NodeADT.h
@@ -1,11 +1,16 @@
 #ifndef NODEADT_H
 #define NODEADT_H
 
+#include <iostream>
+
+using namespace std;
+
 template <typename T>
 class NodeADT {
 	public:
-		virtual void insert(T* obj) = 0;
-		virtual void search(T* obj) = 0;
+		virtual bool insert(T* obj) = 0;
+		virtual void search(T* obj, const ostream& out) = 0;
 		virtual void remove(T* obj) = 0;
+		virtual void dump() = 0;
 };
 #endif
