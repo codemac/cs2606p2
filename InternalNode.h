7a9e38e75be5b626c2fa40d42b3c21ee4c7ca562
buzzfett <buzzfett@a5beea13-9b1e-0410-9abb-f3fe362f9339>
2006-10-12 01:17:01 +0000

    InsertToLeaf

--- InternalNode.h
+++ InternalNode.h
@@ -14,12 +14,14 @@ class InternalNode : public NodeADT {
 
 		NodeADT *one, *two, *three;
 
+		virtual InternalNode<T,D,C>* insertToLeaf(T* obj);
+		virtual InternalNode<T,D,C>* insertToInternal(T* obj);
 	public:
 		InternalNode();
-		InternalNode(D* d, NodeADT* chil1, NodeADT* chil2);
+		InternalNode(D* disc1, D* disc2, NodeADT* chil1, NodeADT* chil2, NodeADT* chil3);
 		~InternalNode();
 
-		virtual bool insert(T* obj);
+		virtual InternalNode<T,D,C>* insert(T* obj);
 		virtual void search(T* obj, const ostream& out);
 		virtual void remove(T* obj);
 		virtual void dump();
@@ -29,6 +31,71 @@ class InternalNode : public NodeADT {
 template <typename T, typename D, typename C>
 void InternalNode<T,D,C>::dump() {
 	
+}
+
+template <typename T, typename D, typename C>
+InternalNode<T,D,C>* InternalNode<T,D,C>::insert(T* obj) 
+{
+	if ( chil1->isLeaf() ) return insertToLeaf(obj);
+	
+	return insertToInternal(obj);
+}
+
+template <typename T, typename D, typename C>
+InternalNode<T,D,C>* InternalNode<T,D,C>::insertToLeaf(T* obj) 
+{
+	if ( C::lt(*obj, *lDiscrim) )
+	{
+		LeafNode<T,C>* leaf = one->insert(obj);
+		if ( temp )
+		{			
+			if ( three )
+			{
+				InternalNode<T,C>* temp = new InternalNode(rDiscrim, NULL, two, three, NULL);
+				two = leaf;
+				three = NULL;
+				return temp;
+			}
+			else
+			{
+				three = two;
+				two = leaf;
+				lDiscrim = two->lObject->getDiscrim();
+				rDiscrim = three->lObject->getDiscrim();
+			}
+		}
+		else return NULL;
+	}
+	else if ( C::lt(*obj, *rDiscrim) )
+	{
+		LeafNode<T,C>* leaf = two->insert(obj);
+		if ( temp )
+		{
+			if ( three )
+			{
+				InternalNode<T,C>* temp = new InternalNode(rDiscrim, NULL, leaf, three, NULL);
+				three = NULL;
+				return temp;
+			}
+			else
+			{
+				three = leaf;
+				rDiscrim = three->lObject->getDiscrim();
+			}
+		}
+		else return NULL;
+	}
+	else
+	{
+		LeafNode<T,C>* leaf = three->insert(obj);
+		if ( temp )
+		{
+			InternalNode<T,C>* temp = new InternalNode(rDiscrim, NULL, three, leaf, NULL);
+			three = NULL;
+			return temp;
+		}
+		else return NULL;
+	}
 }
 
 #endif
--- LeafNode.h
+++ LeafNode.h
@@ -10,42 +10,45 @@ using namespace std;
 template <typename T, typename C>
 class LeafNode : public NodeADT {
 	private:
-		T* rObject;
 		T* lObject;
+		T* rObject;
 		
 		LeafNode<T,C>* sibling;
+		virtual LeafNode<T,C>* split(T* obj);
 
 	public:
 		LeafNode();
-		LeafNode(T* robj, T* lobj, LeafNode<T,C>* sibling);
+		LeafNode(T* lobj, T* robj, LeafNode<T,C>* sibling);
 		~LeafNode();
 
-		virtual bool insert(T* obj); 
+		virtual LeafNode<T,C>* insert(T* obj); 
 		virtual void search(T* obj, const ostream& out);
 		virtual void remove(T* obj);
+		virtual bool isFull();
 		virtual void dump();
+		
 
 		friend class InternalNode;
 };
 
 template <typename T, typename C>
-bool LeafNode<T,C>::insert(T* obj) {
+LeafNode<T,C>* LeafNode<T,C>::insert(T* obj) {
 	if ( lObject == 0 ) {
 		lObject = obj;
-		return true;
+		return NULL;
 	}
 	else if ( rObject == 0 ) {
-		if ( C::lte(*lObject, *obj) ) {	//		lObject <= obj
+		if ( C::lt(*lObject, *obj) ) {	//		lObject <= obj
 			rObject = obj;
 		}
 		else {
 			rObject = lObject;
 			lObject = obj;
 		}
-		return true;
+		return NULL;
 	}
 	else {
-		return false;
+		return split(obj);
 	}
 }
 
@@ -58,4 +61,41 @@ void LeafNode<T,C>::dump() {
 	C::dump(lObject);
 	C::dump(rObject);
 }
+
+template <typename T, typename C>
+bool LeafNode<T,C>::isFull()
+{
+	if ( lObject == 0 && rObject == 0 ) return true;
+	return false;
+}
+
+template <typename T, typename C>
+LeafNode<T,C>* LeafNode<T,C>::split(T* obj)
+{
+	if ( !isFull() ) return NULL;
+	
+	if ( C::lt(obj*, lObject*) )
+	{
+		LeafNode<T,C>* temp = new LeafNode<T,C>(lObject, rObject, NULL);
+		sibling = temp;
+		rObject = NULL;
+		lObject = obj;
+		return temp;
+	}
+	else if ( C::lt(rObject*, obj*) )
+	{
+		LeafNode<T,C>* temp = new LeafNode<T,C>(rObject, obj, NULL);
+		sibling = temp;
+		rObject = NULL;
+		return temp;
+	}
+	else
+	{
+		LeafNode<T,C>* temp = new LeafNode<T,C>(obj, rObject, NULL);
+		sibling = temp;
+		rObject = NULL;
+		return temp;
+	}		
+}
+
 #endif
