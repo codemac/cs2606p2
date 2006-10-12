ed9f0b048b243c5cecf227b5085386b2bbd0ce37
codemac <codemac@a5beea13-9b1e-0410-9abb-f3fe362f9339>
2006-10-12 19:09:35 +0000

    Started the system class

---
+++ trunk/System.h
@@ -0,0 +1,17 @@
+#ifndef SYSTEM_H
+#define SYSTEM_H
+
+#include "KDTree.h"
+#include "BTree.h"
+#include "Parser.h"
+
+using namespace std;
+
+class System {
+	private:
+		Parser p;
+		KDTree 
+	public:
+		void run();
+};
+#endif
