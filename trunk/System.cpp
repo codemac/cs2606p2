bef64a591d97f5b6df4c25362bcff10aefd905bd
codemac <codemac@a5beea13-9b1e-0410-9abb-f3fe362f9339>
2006-10-13 01:24:34 +0000

    Almost done with command handling

--- trunk/System.cpp
+++ trunk/System.cpp
@@ -18,7 +18,38 @@ void System::run(const ostream& out) {
         {
             
 			Record* r = new Record();
+			int id;
+			cin >> id;
+			r->id(id);
 
+			string title;
+			cin.getline(title, 100);
+			r->title(title);
+
+			cin.getline(title, 100);
+			istringstream stream(title);
+			int date;
+			stream >> date;
+			r->date(date);
+
+			int length;
+			stream >> length;
+			r->length(length);
+
+			int x,y;
+			stream >> x >> y;
+			r->x(x);	r->y(y);
+
+			int cost;
+			stream >> cost;
+			r->cost(cost);
+
+			while (cin.peek() != '\n')
+			{
+				cin >> title;
+				r->keywords(title);
+			}
+			//	Description insert goes here.
         }
         else if ( command == "delete" )
         {
@@ -40,7 +71,14 @@ void System::run(const ostream& out) {
 			}
 			else if ( command == "keyword" )
 			{
-				
+				Record* o = new Record();
+				while ( cin.peek() != '\n' )
+				{
+					cin >> command;
+					o->keywords(command);
+				}
+				keyword.search(o,cout);
+					
 			}
 			else if ( command == "location" )
 			{
@@ -49,17 +87,28 @@ void System::run(const ostream& out) {
 				Record *o1 = new Record();
 				o1->x(x);
 				o1->y(y);
-				kdtree.insert(o1);
+				location.search(o1,cout);
 			}
 			else if ( command == "cost" )
 			{
-
+				int low, high;
+				cin >> low >> high;
+				Record *o1 = new Record();
+				Record *o2 = new Record();
+				o1->cost(low);
+				o2->cost(high);
+				cost.search(o1,o2,cout);
 			}
         }
         else if ( command == "dump" )
         {
-            bst.display(cout);
-            kdt.display(cout);
+            cin >> command;
+			if ( command == "date" ) date.dump();
+			else if ( command == "keyword" ) keyword.dump();
+			else if ( command == "location" ) location.dump();
+			else if ( command == "cost" ) cost.dump();
+			else if ( command == "ID" ) id.dump();
+			else cout << "Invalid command" << endl;
         }
         else if ( command == "exit" || command == "quit" )
         {
