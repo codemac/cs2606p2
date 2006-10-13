a29919117fffe9bc3aec6962ea0e708d60daa349
codemac <codemac@a5beea13-9b1e-0410-9abb-f3fe362f9339>
2006-10-13 02:56:56 +0000

    done?

--- trunk/p2.cpp
+++ trunk/p2.cpp
@@ -1,4 +1,9 @@
 /**
+ * Honor Code Statement:
+ *		On our honors as Virginia Polytechnic Institute and State University
+ *		students, we worked as a pair, taking no code from other sources other
+ *		than our own thoughts.  We did not cheat on this project.
+ *
  * @author codemac, buzzfett
  * @version 20061012
  */
